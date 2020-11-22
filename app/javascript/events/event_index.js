327e31d3364ae9487c7a23cdd1556fb936ad8ed9
rwbykz <dreamervt8@gmail.com>
2020-11-22 15:39:21 +0600

    js turbolinks fix

--- app/javascript/events/event_index.js
+++ app/javascript/events/event_index.js
@@ -1,5 +1,5 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', ready);
+document.addEventListener('turbolinks:load', ready);
 
 function ready() {
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("packs/events/event_index")
+
+require("events/event_index")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
